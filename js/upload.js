17bc0576aa017d2dc92e3faa14993a30b487b64b
Bektemir1 <kudajberdievbektemir@gmail.com>
2021-02-12 18:11:08 +0600

    modified optimization

--- js/upload.js
+++ js/upload.js
@@ -1,58 +1,69 @@
 $(document).ready(()=>{
    $('#modal-btn').click(()=>{
-        $('.upload-section').css({'display':'block'})
-   })
-   $('#close').click(()=>{
-       $('.upload-section').css({"display": "none"})
-   })
-})
+        $('.upload-section').css({"display":"block"})
+    })
+    $('#close-content').click(()=>{
+      $('.upload-section').css({"display": "none" })
+      
+    })
+ })
 
-$(document).ready(function() {
-  $('.form img').click(()=>{
-    $('#file-upload').click();
-  })
-  $('.error-img').click(()=>{
-    $('#file-upload').click();
-  })
-  $('.last-form img').click(()=>{
+ $(document).ready(function() {
+  $('.loading-text').click(()=>{
     $('#file-upload').click();
       
   })
-
-
+$('.close-file').click(()=>{
+      $('.file-info').css({"display":"none"})
+  })
+const arrayFile = [];
   const handleFiles = (files) => {
   const selectedFiles = [...files];
   selectedFiles.map((item)=>{
-        const fileName = item.name;
-        const fileSize = item.size;
-        checkExtenstion(fileName)
-        $('.names').text(fileName).css({"font-weight":"600"});
-        $('.size').text(fileSize+" KB");
-        $('.progress-name').text(fileName);
-        $('.info-img').css({"display": "inline-block"})
-        $('.file-info').css({"display":"flex"})
-  })
-  
+            const fileName = item.name;
+            const fileSize = item.size;
+        
+              arrayFile.push(fileName)
+              console.log(arrayFile[arrayFile.length-1])
+              console.log(arrayFile)
+              checkExtenstion(fileName)
+              $('.names').text(fileName).css({"font-weight":"600"});
+              $('.size').text(fileSize+" KB");
+              $('.progress-name').text(fileName);
+    })
 }
-$('#file-upload').change(function(e){
-      handleFiles(e.target.files)
-       });
-  });
-
-  function checkExtenstion(file){
+$('#file-upload[type=file]').change(function(e){
+    handleFiles(e.target.files)
+      });
+ 
+   
+   
+    function checkExtenstion(file){
     const arrayFile = file.split('');
     const indexDot = arrayFile.indexOf('.');
     const sliceArray = arrayFile.slice(indexDot).join('')
-    if(sliceArray !== '.docx'){
-      $('.error-block').css({"display": "block"})
-      $('.form').css({"display": "none"})
-      $('.last-form').css({"display":"none"})
-      }
-      else{
-       
-        move()
-      }
+  
+      if(sliceArray !== '.xlsx'){
+    $('.error-block').css({"display": "block"})
+    $('.form').css({"display": "none"})
+    $('.last-form').css({"display":'none'})
+    $('.file-info').css({"display":"none"})
+    setTimeout(()=>{
+      $('.error-block').css({"display": "none"})
+      $('.last-form').css({"display": "block"})
+ 
+    },2000)
   }
+
+  else{
+    move()
+        
+  
+  }
+ }
+  
+
+  
 
   var i = 0;
    let count = 0;
@@ -69,9 +80,11 @@ $('#file-upload').change(function(e){
             setTimeout(()=>{
              $('.success-block').css({"display":"none"})
              $('.last-form').css({"display":'block'})
+            $('.file-info').css({"display":"block"})
           
 
              },2000)
+          
            clearInterval(id);
            $('.progressing').css({"display": "none"})
            i = 0;
@@ -89,17 +102,10 @@ $('#file-upload').change(function(e){
            $('.error-block').css({"display": "none"})
            $('.last-form').css({"display":"none"})
            $('.progressing').css({"display": "block"}) 
-           
-           $('.close-progress').click(()=>{
-            $('.form').css({"display": "block"})
-            $('.error-block').css({"display": "none"})
-            $('.last-form').css({"display":"none"})
-            $('.progressing').css({"display": "none"}) 
-           })
-           
-         }
+           }
         
-
-       }
+          }
      }
    }
+  });
+
